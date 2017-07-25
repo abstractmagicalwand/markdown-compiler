5f85f97f08fe90d10caad102beb0931562b37d6c
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-07-25 12:20:15 +0300

    Init

---
+++ constants.js
@@ -0,0 +1,11 @@
+const tokens = Object.freeze({
+  UNDERSCORE: 'UNDERSCORE',
+  ASTERISK: 'ASTERISK',
+  CHARS: 'CHARS',
+  GREATER: 'GREATER',
+  NEW_LINE: 'NEW_LINE',
+})
+
+module.exports = {
+  tokens,
+}
