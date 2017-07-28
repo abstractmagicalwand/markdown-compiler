931d685ec95109ad77125a5525867418f2f4fd88
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-07-28 13:34:04 +0300

    Write test for Node

---
+++ node.test.js
@@ -0,0 +1,56 @@
+/* eslint
+  comma-dangle: 0,
+  no-magic-numbers: 0
+*/
+
+import test from 'ava'
+import {tokens, nodes} from './constants'
+import Node from './node'
+import forEach from 'lodash/forEach'
+
+test('create Node', t => {
+  const input = {
+    [tokens.ASTERISK]: {
+      type: tokens.ASTERISK,
+      value: '*',
+      start: 0,
+      end: 1,
+    },
+    [tokens.UNDERSCORE]: {
+      type: tokens.UNDERSCORE,
+      value: '_',
+      start: 2,
+      end: 3,
+    },
+  }
+
+  const expected = {
+    [tokens.ASTERISK]: {
+      type: nodes.ITALIC,
+      operator: '*',
+      closed: false
+    },
+    [tokens.UNDERSCORE]: {
+      type: nodes.ITALIC,
+      operator: '_',
+      closed: false,
+    },
+  }
+
+  const output = {
+    [tokens.ASTERISK]: new Node(input[tokens.ASTERISK]),
+    [tokens.UNDERSCORE]: new Node(input[tokens.UNDERSCORE]),
+  }
+
+  forEach(output, (value, type) => forEach(
+    expected[type],
+    (value, prop) => t.is(
+      output[type][prop],
+      expected[type][prop],
+      `${type}.${prop}`
+    )
+  ))
+
+  t.is(output[tokens.ASTERISK].length, 0, `${tokens.ASTERISK}.body`)
+  t.is(output[tokens.UNDERSCORE].length, 0, `${tokens.UNDERSCORE}.body`)
+})
