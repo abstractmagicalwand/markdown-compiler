2b329125b6e9a3509b6ca74e3d829e1153d0e6d9
abstractmagicalwand <abstractmagicalwand@gmail.com>
2017-07-30 09:28:06 +0300

    Create launcher()

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = () => null
+const flow = require('lodash/flow')
+const lexer = require('./lexer')
+const parser = require('./parser')
+const compiler = require('./compiler')
+
+const launcher = flow(lexer, parser, compiler)
+
+module.exports = launcher
